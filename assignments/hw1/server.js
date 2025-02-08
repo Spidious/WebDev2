f7b22a6f8ce19f4da6d5ebeb0983b7dad18efce5
spidious <lukedeffenbaugh@gmail.com>
2025-02-07 23:12:22 -0600

    Submit hw1

--- assignments/hw1/server.js
+++ assignments/hw1/server.js
@@ -1,10 +1,13 @@
+
+// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";this is a test
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
-app.set('layout', 'layouts/main'); // Add this line
+app.set('layout', 'layouts/layout'); // Add this line
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +15,18 @@ app.use(express.urlencoded({ extended: true }));
 // Create app variables
 let messages = [];
 let otherUsers = [];
+let movies =[
+    {
+        title: 'A New Hope',
+        stars: ['Mark Hamill', 'Carrie Fisher', 'Harrison Ford'],
+        director: 'George Lucas'
+    }, 
+    {
+        title: 'The Empire Strikes Back',
+        stars: ['Mark Hammil', 'Carrie Fischer', 'Harrison Ford'],
+        director: 'George Lucas'
+    }, 
+];
 const user = {pawprint: 'ltdckd', firstname: 'Luke', lastname: 'Deffenbaugh'};
 
 // Create route for home
@@ -34,6 +49,24 @@ app.post('/about-submit', (req, res) => {
     const bio = req.body.userBio;
     otherUsers.push({name: name, bio: bio});
     res.redirect('/about');
+})
+
+// Create route for movies
+app.get('/movies', (req, res) => {
+    res.render('movies', { title: 'Movies', movies, user});
+});
+app.post('/movies-submit', (req, res) => {
+    const title = req.body.title;
+    const stars_raw = (req.body.stars).split(',');
+    const director = req.body.director;
+
+    let stars = [];
+    stars_raw.forEach(function(star) {
+        stars.push(star);
+    });
+
+    movies.push({title: title, stars: stars, director: director});
+    res.redirect('/movies');
 })
 
 
